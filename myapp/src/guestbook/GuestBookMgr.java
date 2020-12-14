0e9cc9a90a1520587df7f82d1a493cd7351a6495
ejatkdml <73155757+ejatkdml@users.noreply.github.com>
2020-12-14 13:52:39 +0900

    수업자료

---
+++ myapp/src/guestbook/GuestBookMgr.java
@@ -0,0 +1,113 @@
+package guestbook;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Vector;
+
+public class GuestBookMgr {
+	
+	private DBConnectionMgr pool;
+	private final SimpleDateFormat SDF_DATE =
+			new SimpleDateFormat("yyyy'³â'  M'¿ù' d'ÀÏ' (E)");
+	private final SimpleDateFormat SDF_TIME =
+			new SimpleDateFormat("H:mm:ss");
+	
+	public GuestBookMgr() {
+		pool = DBConnectionMgr.getInstance();
+	}
+	
+	//Join Login
+	public boolean loginJoin(String id, String pwd) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		boolean flag = false;
+		try {
+			con = pool.getConnection();
+			sql = "select * from tblJoin where id=? and pwd =?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			if(pstmt.executeQuery().next())
+				flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return flag;
+	}
+	
+	//Join Information
+	public JoinBean getJoin(String id) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		JoinBean bean = new JoinBean();
+		try {
+			con = pool.getConnection();
+			sql = "select * from tblJoin where id=?";
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if(rs.next()){
+				bean.setId(rs.getString(1));
+				bean.setPwd(rs.getString(2));
+				bean.setName(rs.getString(3));
+				bean.setEmail(rs.getString(4));
+				bean.setHp(rs.getString(5));
+				bean.setGrade(rs.getString(6));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt, rs);
+		}
+		return bean;
+	}
+	
+	//GuestBook Insert
+	public void insertGuestBook(GuestBookBean bean) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		String sql = null;
+		try {
+			con = pool.getConnection();
+			sql = "";
+			pstmt = con.prepareStatement(sql);
+			int cnt = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			pool.freeConnection(con, pstmt);
+		}
+		return;
+	}
+	
+	//GuestBook List
+	
+	//GuestBook Read
+	
+	//GuestBook Update : Content, ip, secret
+	
+	//GuestBook Delete
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
